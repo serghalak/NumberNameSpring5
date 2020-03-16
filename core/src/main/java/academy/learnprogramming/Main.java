874274194c262db0fea806ce91b77d01d3baa48d
serghalak <serghalak@gmail.com>
2020-03-16 11:19:11 +0200

    add xml configuration

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,13 +2,26 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private final static Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION="bean.xml";
 
     public static void main(String[] args) {
         log.info("Guess the number Game");
         System.out.println("Main ...........");
+
+        ApplicationContext context=new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number=numberGenerator.next();
+        log.info("number = {}",number);
+        System.out.println("Max Number is: " + numberGenerator.getMaxNumber() + " random number is: " + numberGenerator.next());
+
+        //context.close();
+
     }
 
 }
