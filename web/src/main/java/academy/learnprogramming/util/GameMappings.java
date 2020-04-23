ca2548539029a00f48766d83617059fcd70b720d
serghalak <serghalak@gmail.com>
2020-04-23 19:36:19 +0300

    add game-over view use preprocessor

--- web/src/main/java/academy/learnprogramming/util/GameMappings.java
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -4,7 +4,8 @@ public final class GameMappings {
 
     public static final String PLAY="play";
     public static final String REDIRECT_PLAY="redirect:/" + PLAY;
-
+    public static final String RESTART="restart";
+    public static final String HOME="/";
 
     private GameMappings() {
     }
